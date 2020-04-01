6c1cf4390125ebb532d5d2611936a07ded18b44b
pufftano <pufftano@outlook.com>
2020-04-01 20:37:28 +0200

    Added login and register functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,86 @@
 import 'package:flutter/material.dart';
-import 'screens/login_signup.dart';
-import 'screens/take.dart';
 
-void main() => runApp(MyApp());
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+import 'package:help_giver/features/userhandling/presentation/pages/homepage.dart';
+import 'package:help_giver/features/userhandling/presentation/pages/splash_page.dart';
+import 'package:help_giver/features/userhandling/presentation/pages/login_page.dart';
+import 'package:help_giver/features/userhandling/presentation/pages/register_page.dart';
+
+import 'package:help_giver/features/userhandling/data/repositories/userhandling_repo.dart';
+
+import 'package:help_giver/features/userhandling/presentation/bloc/authentication_bloc.dart';
+import 'package:help_giver/features/userhandling/presentation/widgets/loading_indicator.dart';
+
+import 'package:help_giver/features/userhandling/domain/usecases/authentication_usecase.dart';
+import 'package:help_giver/features/userhandling/domain/entities/authentication_entity.dart';
+
+
+class SimpleBlocDelegate extends BlocDelegate {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TakeHomePage(),
-    );
+  void onTransition(Transition transition) {
+    print(transition.toString());
   }
 }
 
-class MyHomePage extends StatelessWidget {
+void main() {
+  BlocSupervisor().delegate = SimpleBlocDelegate();
+  runApp(App(userRepository: UserRepository()));
+}
+
+class App extends StatefulWidget {
+  final UserRepository userRepository;
+
+  App({Key key, @required this.userRepository}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  AuthenticationBloc authenticationBloc;
+  UserRepository get userRepository => widget.userRepository;
+
+  @override
+  void initState() {
+    authenticationBloc = AuthenticationBloc(userRepository: userRepository);
+    authenticationBloc.dispatch(AppStarted());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    authenticationBloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: LoginSignup(),
+    return BlocProvider<AuthenticationBloc>(
+      bloc: authenticationBloc,
+      child: MaterialApp(
+        home: BlocBuilder<AuthenticationEvent, AuthenticationState>(
+          bloc: authenticationBloc,
+          builder: (BuildContext context, AuthenticationState state) {
+            if (state is AuthenticationUninitialized) {
+              return SplashPage();
+            }
+            if (state is AuthenticationAuthenticated) {
+              return HomePage();
+            }
+            if (state is AuthenticationUnauthenticated) {
+              return LoginPage(userRepository: userRepository);
+            }
+            if (state is AuthenticationStartRegistration) {
+              return RegisterPage(userRepository: userRepository);
+            }
+            if (state is AuthenticationLoading) {
+              return LoadingIndicator();
+            }
+          },
+        ),
+      ),
     );
   }
 }
